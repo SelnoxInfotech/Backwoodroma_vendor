7942f966a819fd7fa64d8361bfc035210438618f
swapnil9340 <95558311+swapnil9340@users.noreply.github.com>
2023-02-24 23:39:47 +0530

    ll

---
+++ src/Components/Page/Store/StoreAdd/StoreAdd.jsx
@@ -0,0 +1,94 @@
+import { TextField } from "@material-ui/core";
+import React from "react";
+
+export default function StoreAdd() {
+    return (
+        <div>
+            <div className='container-fluid Border pading'>
+                <div className='row   '>
+                    <div className='col-12  '>
+                        <div className='col-6   signup_margin'>
+                            <span>STORE DETAILS</span>
+                        </div>
+                        <div className="col-12 login_width  signup_field"   >
+                            <div className="container-fluid">
+                                <div className=" row gap justify-content-center">
+                                    <div className='col-12  signup_Display center'>
+                                        <div className="col-sm-4 center pading">
+                                            <div className='col-4 name_style'>
+                                                <span className='icon'>x</span>
+                                                <span>Store Name</span>
+                                            </div>
+                                        </div>
+
+                                        <div className='col-sm-8 '>
+                                            <TextField
+                                                variant="filled"
+                                                fullWidth
+                                            ></TextField>
+                                        </div>
+                                    </div>
+                                    <div className='col-12  signup_Display center'>
+                                        <div className="col-sm-4 center pading">
+                                            <div className='col-4 name_style'>
+                                                <span className='icon'> </span>
+                                                <span>Store Type</span>
+                                            </div>
+                                        </div>
+
+                                        <div className='col-sm-8 '>
+                                            <TextField
+                                                variant="filled"
+                                                fullWidth
+                                            ></TextField>
+                                        </div>
+                                    </div>
+                                    <div className='col-12  signup_Display '>
+                                        <div className="col-sm-4 center pading">
+                                            <div className='col-4 name_style'>
+                                                <span className='icon'> </span>
+                                                <span>Country</span>
+                                            </div>
+                                        </div>
+
+                                        <div className='col-sm-2 '>
+                                            <TextField
+                                                variant="filled"
+                                                fullWidth
+                                            ></TextField>
+                                        </div>
+                                        <div className="col-6 signup_Display">
+                                            <div className='col display name_style'>
+                                                <span className='icon'> </span>
+                                                <span>Country</span>
+                                            </div>
+                                            <div className='col-sm-2 '>
+                                                <TextField
+                                                    variant="filled"
+                                                    fullWidth
+                                                ></TextField>
+                                            </div>
+                                            <div className='col display name_style'>
+                                                <span className='icon'>x </span>
+                                                <span>Country</span>
+                                            </div>
+                                            <div className='col-sm-2 '>
+                                                <TextField
+                                                    variant="filled"
+                                                    fullWidth
+                                                ></TextField>
+                                            </div>
+                                      </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- src/Routes/Routing/Routing.jsx
+++ src/Routes/Routing/Routing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes    } from "react-router-dom";
 import SignUp from '../../Components/Page/Signup/SignUp';
 import Login from "../../Components/Page/Login/Login"
+import StoreAdd from '../../Components/Page/Store/StoreAdd/StoreAdd';
 
 
 export default function Router() {
@@ -14,7 +15,7 @@ export default function Router() {
         <Routes  >
          <Route  path="/" element={<SignUp></SignUp>}/>
          <Route  path="/Login" element={<Login></Login>}/>
-       
+         <Route path="/StoreAdd" element={<StoreAdd></StoreAdd>} />
         </Routes>
       </BrowserRouter>
       
