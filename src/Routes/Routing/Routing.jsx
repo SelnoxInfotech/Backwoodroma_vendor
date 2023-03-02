f58d2af2765357039162d38b512ab4031a87f82f
Chandrajeet123 <selnoxreact@gmail.com>
2023-03-02 10:31:46 +0530

    change

--- src/Routes/Routing/Routing.jsx
+++ src/Routes/Routing/Routing.jsx
@@ -18,6 +18,7 @@ export default function Router() {
          <Route path="/StoreAdd" element={<StoreAdd></StoreAdd>} />
          <Route path='/Dashboard' element={<Dashboard/>}/>
         </Routes>
+        
       </BrowserRouter>
       
   
