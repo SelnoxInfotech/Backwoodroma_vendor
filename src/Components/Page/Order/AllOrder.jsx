0ebe7542bf9979c92de8e74033f18712a3e8e220
Chandrajeet123 <selnoxreact@gmail.com>
2023-03-10 19:13:03 +0530

    all_order

--- src/Components/Page/Order/AllOrder.jsx
+++ src/Components/Page/Order/AllOrder.jsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import {IoRadioButtonOnSharp} from "react-icons/io"
+import { IoRadioButtonOnSharp } from "react-icons/io5"
+import Box from '@mui/material/Box';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#fffff",
+        },
+
+
+
+    },
+
+});
 const AllOrder = () => {
-    const arrData = [{ id: "Order Id", order_num: "#12340", delivery: "Delivery"}, { id: "Order Id2", order_num: "#12340", delivery: "Delivery2" }]
+    const arrData = [{ id: "Order Id", order_num: "#12340", delivery: "Delivery", icons: <IoRadioButtonOnSharp /> ,location:"Jakarta",date:"3/10",to:"Singapore",rec_date:"10/10"}, { id: "Order Id2", order_num: "#12340", delivery: "Delivery2",location:"Jakarta",date:"3/10",to:"Singapore",rec_date:"10/10" }]
     return (
         <>
+            <ThemeProvider theme={theme}>
+
             <div className="container-fluid">
                 <div className="row border mt-4">
                     {arrData.map((ele, index) => {
@@ -15,11 +30,40 @@ const AllOrder = () => {
                                     <div className="all_order_card_head">
                                         <div className="d-flex "> <p>{ele.id}</p><span className="all_order_span">{ele.order_num}</span></div>
                                         <Stack direction="row" spacing={1}>
-                                            <Chip label={ele.delivery}  variant="outlined" />
+                                            <Chip label={ele.delivery} variant="outlined" icon={<IoRadioButtonOnSharp />} />
                                         </Stack>
+                                    </div>
+                                    <div className='all_order_card_head'>
+
+                                        <div className='all_order_card'>
+                                            <Box sx={{ 
+                                                "&.svg":{
+                                                 color:"green"
+                                                }
+                                            }
+                                            }
+                                                
+                                                >
+                                                
+                                            <IoRadioButtonOnSharp />
+
+                                            <span className='all_order_span'>{ele.location}</span>
+                                            </Box>
+
+                                            </div>
+                                        <p className='all_order_span'>{ele.date}</p>
+                                    </div>
+                                    <div className='all_order_card_head'>
+
+                                        <div className='all_order_card'>
+                                            <IoRadioButtonOnSharp />
+                                            <span className='all_order_span'>{ele.to}</span>
+                                            </div>
+                                        <p className='all_order_span'>{ele.rec_date}</p>
                                     </div>
 
                                 </div>
+
 
                             </div>
                         )
@@ -29,6 +73,7 @@ const AllOrder = () => {
 
 
             </div>
+            </ThemeProvider>
         </>
     )
 }
