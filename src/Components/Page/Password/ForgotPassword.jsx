15ff48162bb456a193122fd4cf05dfa442fc018e
Chandrajeet123 <selnoxreact@gmail.com>
2023-03-14 15:35:28 +0530

    forgot

--- src/Components/Page/Password/ForgotPassword.jsx
+++ src/Components/Page/Password/ForgotPassword.jsx
@@ -3,13 +3,24 @@ import TextField from '@mui/material/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import { CiUser } from "react-icons/ci"
 import {AiOutlineMail} from "react-icons/ai"
+import useStyles from "../../../../src/Style"
+import LoadingButton from '@mui/lab/LoadingButton';
+import Box from '@mui/material/Box';
 
 const ForgotPassword = () => {
+    const classes=useStyles()
     return (
         <>
             <div className="container-fluid forgot_passwrd_cont">
                 <div className='  forgotPwd_main_div'>
-                    <Paper sx={{ "&.MuiPaper-root ": { height: "70%" } }} elevation={3}>
+                    <Paper 
+                    className={classes.papers}
+                    // sx={{
+                    //      "&.MuiPaper-root ": { height: "70%" }
+                         
+                    //      }} 
+                         elevation={3}
+                         >
                         <div className='row center forgot_head'>
                             <div className='col-lg-12'>
                                 <h6>Forgot Password ?</h6>
@@ -28,9 +39,8 @@ const ForgotPassword = () => {
                                 <div className='col-lg-12'>
                                 <TextField id="standard-basic" variant="standard" placeholder='Enter Your Email'
                                     fullWidth
-                                    sx={{
-                                        '.MuiInputBase-input': { fontSize: '.7rem' },
-                                    }}
+                                    className={classes.textFieldCss}
+                                 
                                     InputProps={{
                                         startAdornment: (
                                             <InputAdornment position="start">
@@ -43,6 +53,34 @@ const ForgotPassword = () => {
                                 </div>
 
                             </div>
+                            <div className='row mt-2'>
+                                <div className='col-lg-12 center '>
+                                    <Box 
+                                    // className={"mt-4 mb-4"+`${}`}
+                                    // className={"mt-4 mb-4 newLogin_btn_paper"+} 
+                                    // sx={{
+
+                                    //     ".MuiButton-text": {
+                                    //         color: "white",
+                                    //         fontSize: ".7rem"
+                                    //     }
+                                    // }}
+                                    
+                                    >
+                                        <LoadingButton
+                                            type="submit"
+                                          
+                                        >
+                                           Reset Password
+                                        </LoadingButton>
+                                    </Box>
+
+                                </div>
+
+
+                            </div>
+                           
+
                         </form>
 
                     </Paper>
--- src/Style.jsx
+++ src/Style.jsx
@@ -5,6 +5,19 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
     customHoverFocus: {
       "&:hover, &.Mui-focusVisible": {   backgroundColor: '#f00' }
+    },
+    papers:{
+      "&.MuiPaper-root ":{
+        height: "70%",
+       
+       
+      }
+    },
+    textFieldCss:{
+      "& .MuiInputBase-input":{
+        fontSize: '.7rem',
+    
+      }
     }
   }));
 
